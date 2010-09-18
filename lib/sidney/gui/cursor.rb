a082222b656d7d0f7f3d5e691775e77cc95df69e
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-09-18 19:20:26 +0100

    Prevent mouse being shown twice (the game drawing the mouse when the system is drawing it).

--- lib/sidney/gui/cursor.rb
+++ lib/sidney/gui/cursor.rb
@@ -1,17 +1,31 @@
 module Sidney
 class Cursor < GameObject
+  protected
   def initialize(options = {})
     super({image: "mouse.png", center: 0, zorder: ZOrder::CURSOR}.merge(options))
 
     nil
   end
 
+  # Is the mouse pointer position inside the game window pane?
+  public
+  def inside_window?
+    x >= 0 and y >= 0 and x < $window.width and y < $window.height
+  end
+
+  public
   def update
     self.x, self.y = $window.mouse_x, $window.mouse_y
 
     super
 
     nil
+  end
+
+  public
+  def draw
+    # Prevent system and game mouse from being shown at the same time.
+    super if inside_window?
   end
 end
 end
