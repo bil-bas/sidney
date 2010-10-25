2fedc39a18deeaefe711cf68f4552ae9cb5b6c96
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-10-25 17:54:43 +0100

    Pulled down Button logic to make a new Label class.

--- lib/sidney/gui.rb
+++ lib/sidney/gui.rb
@@ -2,12 +2,14 @@
 
 require_relative 'gui/button'
 require_relative 'gui/clipboard'
+require_relative 'gui/color_picker'
 require_relative 'gui/color_well'
 require_relative 'gui/combo_box'
 require_relative 'gui/cursor'
 require_relative 'gui/gui_state'
 require_relative 'gui/history'
 require_relative 'gui/horizontal_packer'
+require_relative 'gui/label'
 require_relative 'gui/radio_button'
 require_relative 'gui/selection'
 require_relative 'gui/slider'
@@ -15,7 +17,6 @@ require_relative 'gui/text_area'
 require_relative 'gui/toggle_button'
 require_relative 'gui/tool_tip'
 require_relative 'gui/vertical_packer'
-require_relative 'gui/color_picker'
 
 include Sidney::Gui
 
--- lib/sidney/gui/button.rb
+++ lib/sidney/gui/button.rb
@@ -1,59 +1,10 @@
 # encoding: utf-8
 
-require_relative 'element'
+require_relative 'label'
 
 module Sidney
 module Gui
-class Button < Element
-  BACKGROUND_COLOR = Color.rgb(100, 100, 100)
-  BORDER_COLOR = Color.rgb(150, 150, 150)
-  TEXT_COLOR = Color.rgb(255, 255, 255)
-
-  protected
-  # @option options [Gosu::Image, nil] icon (nil)
-  # @option options [String] text ('')
-  def initialize(parent, options = {}, &block)
-    options = {
-      icon: nil,
-      text: ''
-    }.merge! options
-
-    @text = options[:text]
-    @icon = options[:icon]
-    
-    super(parent, options)
-
-    if @icon
-      if @text.empty?
-        rect.width = [@icon.width + padding_x * 2, width].max
-        rect.height = [@icon.height + padding_y * 2, height].max
-      else
-        rect.width = [@icon.width + font.text_width(@text) + padding_x * 3, width].max
-        rect.height = [[@icon.height, font_size].max + padding_y * 2, height].max
-      end
-    else
-      rect.width = [font.text_width(@text) + padding_x * 2, width].max
-      rect.height = [font_size + padding_y * 2, height].max
-    end
-  end
-
-  public
-  def draw
-    $window.draw_box(x, y, width, height, z, BORDER_COLOR, BACKGROUND_COLOR)
-
-    current_x = x + padding_x - 1
-    if @icon
-      @icon.draw(current_x, y + padding_y, z)
-      current_x += @icon.width + padding_x
-    end
-
-    unless @text.empty?
-      font.draw(@text, current_x, y + padding_y, z, 1, 1, TEXT_COLOR)
-    end
-
-    nil
-  end
-
+class Button < Label
   public
   def clicked_left_mouse_button(sender, x, y)
     # TODO: Play click sound?
