194a8dcaa7f3ed0ff70100c9d7b9c3e64a6ec109
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-10-27 18:22:28 +0100

    Added Gui::GridPacker, now used in resource_packer.

--- lib/sidney/resource_browser.rb
+++ lib/sidney/resource_browser.rb
@@ -19,23 +19,23 @@ module Sidney
       super parent, VerticalPacker.new(nil), options
 
       # TODO: Use a side-scrolling text editor.
-      @text_entry = TextArea.new(inner_container, width: 130, max_height: 30)
+      @text_entry = TextArea.new(inner_container, width: 135, max_height: 30)
       @label = Label.new(inner_container)
 
-      # TODO: Use a GridPacker?
-      @object_list = List.new(inner_container, width: 130)
+      group = RadioButton::Group.new(inner_container)
+      @object_grid = GridPacker.new(group, width: 135, num_columns: 3)
 
       @text_entry.subscribe :changed do |sender, text|
-        @object_list.clear
+        @object_grid.clear
 
         # TODO: This search should be _very_ much better...
         records = @type.all
         records.each do |record|
           if text.split(/ +/).all? {|word| record.name =~ /#{word}/i }
-            @object_list.add_item(icon: Icon.new(record.thumbnail), tip: record.name, text: record.name, border_color: Color.rgb(0, 0, 0))
+            Button.new(@object_grid, icon: Icon.new(record.thumbnail), tip: record.name)
           end
         end
-        @label.text = "#{@object_list.size} of #{records.size}"
+        @label.text = "#{@object_grid.size} of #{records.size}"
 
         publish :changed, text
       end
