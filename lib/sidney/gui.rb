4f68d3460040e14914a3695d312352a23452646a
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2010-10-28 16:50:55 +0100

    Removed Gui module and part of the gosu extension, which moved to my Fidgit library.

--- lib/sidney/gui.rb
+++ lib/sidney/gui.rb
@@ -1,25 +1,12 @@
 # encoding: utf-8
 
-require_relative 'gui/button'
-require_relative 'gui/clipboard'
-require_relative 'gui/color_picker'
-require_relative 'gui/color_well'
-require_relative 'gui/combo_box'
-require_relative 'gui/cursor'
-require_relative 'gui/grid_packer'
-require_relative 'gui/gui_state'
-require_relative 'gui/history'
-require_relative 'gui/horizontal_packer'
-require_relative 'gui/icon'
-require_relative 'gui/label'
-require_relative 'gui/list'
-require_relative 'gui/radio_button'
-require_relative 'gui/selection'
-require_relative 'gui/slider'
-require_relative 'gui/text_area'
-require_relative 'gui/toggle_button'
-require_relative 'gui/tool_tip'
-require_relative 'gui/vertical_packer'
+begin
+  require_relative '../../../fidgit/lib/fidgit'
+rescue Exception => ex
+  require 'fidgit'
+end
 
-include Sidney::Gui
+include Fidgit
+
+Fidgit.font_name = File.expand_path(File.join(__FILE__, '..', '..', '..', 'media', 'fonts', 'SFPixelate.ttf'))
 
